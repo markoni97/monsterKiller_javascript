d7259499b5c879e4d6d2c6ca0b7ea0ae0efb8b2c
markoni97 <54212458+markoni97@users.noreply.github.com>
2020-10-13 14:18:26 +0200

    Added attack button functionality

---
+++ assets/scripts/app.js
@@ -0,0 +1,23 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+function attackHandler(){
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+        alert('You have won');
+    } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0){
+        alert('You have lost');
+    } else if(currentMonsterHealth <= 0 && currentPlayerHealth <= 0){
+        alert('Its a draw');
+    }
+}
+
+attackBtn.addEventListener('click', attackHandler);
