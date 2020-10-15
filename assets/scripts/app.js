75174906af127fbc67d3ce7b229cba45d6082f00
markoni97 <54212458+markoni97@users.noreply.github.com>
2020-10-15 13:18:05 +0200

    Added battle log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,11 @@ const HEAL_VALUE = 20;
 
 const MODE_ATTACK = 'ATTACK';
 const MODE_STRONG_ATTACK = 'STRONG ATTACK';
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
+const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
 const enteredValue = prompt('Enter the max life for you and the monster', '100');
 
@@ -15,9 +20,29 @@ if(isNaN(chosenMaxLife) || chosenMaxLife <= 0 ){
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let battleLog = [];
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function writeToLog(ev, val, monsterHealth, playerHealth){
+    let logEntry = {
+        event: ev,
+        value: val,
+        finalMonsterHealth: monsterHealth,
+        finalPayerHealth: playerHealth
+    };
+    if(ev === LOG_EVENT_PLAYER_ATTACK){
+        logEntry.target = 'MONSTER';
+    } else if(ev === LOG_EVENT_PLAYER_STRONG_ATTACK){
+        logEntry.target = 'MONSTER';
+    } else if(ev === LOG_EVENT_PLAYER_HEAL){
+        logEntry.target = 'PLAYER';
+    } else if(ev === LOG_EVENT_MONSTER_ATTACK){
+        logEntry.target = 'PLAYER';
+    } 
+    battleLog.push(logEntry);
+}
 
 function reset(){
     currentMonsterHealth = chosenMaxLife;
@@ -29,6 +54,12 @@ function endRound(){
     const initialPLayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+    writeToLog(
+        LOG_EVENT_MONSTER_ATTACK,
+        playerDamage,
+        currentMonsterHealth,
+        currentPlayerHealth
+    );
 
     if(currentPlayerHealth <= 0 && hasBonusLife){
         hasBonusLife = false;
@@ -40,10 +71,28 @@ function endRound(){
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert('You have won');
+        writeToLog(
+            LOG_EVENT_GAME_OVER,
+            'Player won',
+            currentMonsterHealth,
+            currentPlayerHealth
+        );
     } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0){
         alert('You have lost');
+        writeToLog(
+            LOG_EVENT_GAME_OVER,
+            'Monster won',
+            currentMonsterHealth,
+            currentPlayerHealth
+        );
     } else if(currentMonsterHealth <= 0 && currentPlayerHealth <= 0){
         alert('Its a draw');
+        writeToLog(
+            LOG_EVENT_GAME_OVER,
+            'Its a draw',
+            currentMonsterHealth,
+            currentPlayerHealth
+        );
     }
 
     if(currentPlayerHealth <= 0 || currentMonsterHealth <= 0){
@@ -53,14 +102,23 @@ function endRound(){
 
 function attack(mode){
     let maxDamage;
+    let logEvent;
     if(mode === MODE_ATTACK){
         maxDamage = ATTACK_VALUE;
+        logEvent = LOG_EVENT_PLAYER_ATTACK;
     } else if (mode === MODE_STRONG_ATTACK){
         maxDamage = STRONG_ATTACK;
+        logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
     }
 
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
+    writeToLog(
+        logEvent,
+        damage,
+        currentMonsterHealth,
+        currentPlayerHealth
+    );
     endRound();
 }
 
@@ -83,7 +141,17 @@ function healPLayerHandler(){
 
     increasePlayerHealth(healValue);
     currentPlayerHealth += healValue;
+    writeToLog(
+        LOG_EVENT_PLAYER_HEAL,
+        healValue,
+        currentMonsterHealth,
+        currentPlayerHealth
+    );
     endRound();
+}
+
+function printLogHandler(){
+    console.log(battleLog);
 }
 
 
@@ -91,3 +159,4 @@ function healPLayerHandler(){
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', healPLayerHandler);
+logBtn.addEventListener('click', printLogHandler);
